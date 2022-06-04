4c967083314e58daa46f82f715fd2d25440bb0e0
Aditya Fakhri <adityafakhri03@gmail.com>
2022-06-04 19:53:05 +0700

    [UPDATE] Tambahkan Fungsi active-btn

--- style/app.js
+++ style/app.js
@@ -7,10 +7,10 @@ const allSections = document.querySelectorAll('.main-content');
 function PageTransitions(){
     //Button click active class
     for(let i = 0; i < secBtn.length; i++){
-        secBtn[i].addEventListener('click', () =>{
+        secBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn'
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
         })
     }
 }
